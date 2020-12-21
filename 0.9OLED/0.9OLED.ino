709ceb8ab4e6a6b7a38ce80a6dd341159b4a6c38
chenjunpeng1996 <langhun1996@gmail.com>
2020-12-21 11:30:10 +0800

    第一次提交

---
+++ 0.9OLED/0.9OLED.ino
@@ -0,0 +1,20 @@
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+  u8g2.drawStr(0,10,"Hello World!");	// write something to the internal memory
+  u8g2.sendBuffer();					// transfer internal memory to the display
+  delay(1000);
+}
